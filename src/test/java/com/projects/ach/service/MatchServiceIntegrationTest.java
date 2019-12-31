4ac7fec32eff1754bb85e39cf6e6a27fe697a98a
ABDELCHAG <abdellah.chagdani@gmail.com>
2019-12-31 19:49:53 +0100

    DEV::Sprint2::US2::Tests d'integrations winTieBreak && winSet

--- src/test/java/com/projects/ach/service/MatchServiceIntegrationTest.java
+++ src/test/java/com/projects/ach/service/MatchServiceIntegrationTest.java
@@ -12,8 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.projects.ach.model.Game;
+import com.projects.ach.model.Player;
 import com.projects.ach.model.Point;
 import com.projects.ach.model.Set;
+import com.projects.ach.model.TieBreak;
 
 /**
  * @author ABDELCHAG
@@ -187,6 +189,158 @@ public class MatchServiceIntegrationTest {
 		assertThat(previous.getPointsPlayer2().get(10)).isEqualTo(Point.PDEUCE);
 		assertThat(previous.getWinner()).isEqualTo(set.getPlayer1());
 		assertThat(isFinish).isFalse();
+	}
+
+	@Test
+	public void testScorePointWinSet6To0() {
+		Set set = matchService.startMatch(playerName1, playerName2);
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer1());
+		assertThat(set.getGames()).hasSize(6);
+		for (Game game : set.getGames()) {
+			assertThat(game.getWinner()).isEqualTo(set.getPlayer1());
+		}
+
+		assertThat(set.getWinner()).isNotNull();
+		assertThat(set.getWinner()).isEqualTo(set.getPlayer1());
+		Integer lastScore1 = set.getScoresPlayer1().get(set.getScoresPlayer1().size() - 1);
+		Integer lastScore2 = set.getScoresPlayer2().get(set.getScoresPlayer2().size() - 1);
+		assertThat(lastScore1).isEqualTo(6);
+		assertThat(lastScore2).isEqualTo(0);
+
+	}
+
+	@Test
+	public void testScorePointWinSet7To5() {
+		Set set = matchService.startMatch(playerName1, playerName2);
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer1());
+		assertThat(set.getGames()).hasSize(12);
+
+		assertThat(set.getWinner()).isNotNull();
+		assertThat(set.getWinner()).isEqualTo(set.getPlayer1());
+
+		Integer lastScore1 = set.getScoresPlayer1().get(set.getScoresPlayer1().size() - 1);
+		Integer lastScore2 = set.getScoresPlayer2().get(set.getScoresPlayer2().size() - 1);
+		assertThat(lastScore1).isEqualTo(7);
+		assertThat(lastScore2).isEqualTo(5);
+
+	}
+
+	@Test
+	public void testScorePoint6To5() {
+		Set set = matchService.startMatch(playerName1, playerName2);
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		assertThat(set.getGames()).hasSize(12);
+
+		assertThat(set.getWinner()).isNull();
+
+		Integer lastScore1 = set.getScoresPlayer1().get(set.getScoresPlayer1().size() - 1);
+		Integer lastScore2 = set.getScoresPlayer2().get(set.getScoresPlayer2().size() - 1);
+		assertThat(lastScore1).isEqualTo(6);
+		assertThat(lastScore2).isEqualTo(5);
+
+		Game current = (Game) set.getCurrentGame();
+		assertThat(current.getPointsPlayer1()).hasSize(1);
+		assertThat(current.getPointsPlayer2()).hasSize(1);
+
+	}
+
+	@Test
+	public void testScorePoint6To6() {
+		Set set = matchService.startMatch(playerName1, playerName2);
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		assertThat(set.getGames()).hasSize(12);
+		assertThat(set.getTieBreak()).isNotNull();
+
+		assertThat(set.getWinner()).isNull();
+
+		Integer lastScore1 = set.getScoresPlayer1().get(set.getScoresPlayer1().size() - 1);
+		Integer lastScore2 = set.getScoresPlayer2().get(set.getScoresPlayer2().size() - 1);
+		assertThat(lastScore1).isEqualTo(6);
+		assertThat(lastScore2).isEqualTo(6);
+
+		TieBreak current = (TieBreak) set.getCurrentGame();
+		assertThat(current.getScoresPlayer1()).hasSize(1);
+		assertThat(current.getScoresPlayer2()).hasSize(1);
+
+	}
+
+	@Test
+	public void testScorePoint7To6() {
+		Set set = matchService.startMatch(playerName1, playerName2);
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winGame(set, set.getPlayer1());
+		winGame(set, set.getPlayer2());
+		winTieBreak(set, set.getPlayer1());
+		assertThat(set.getGames()).hasSize(12);
+		assertThat(set.getTieBreak()).isNotNull();
+		assertThat(set.getTieBreak().getWinner()).isNotNull();
+		assertThat(set.getTieBreak().getWinner()).isEqualTo(set.getPlayer1());
+
+		assertThat(set.getWinner()).isNotNull();
+		assertThat(set.getWinner()).isEqualTo(set.getPlayer1());
+
+		Integer lastScore1 = set.getScoresPlayer1().get(set.getScoresPlayer1().size() - 1);
+		Integer lastScore2 = set.getScoresPlayer2().get(set.getScoresPlayer2().size() - 1);
+		assertThat(lastScore1).isEqualTo(7);
+		assertThat(lastScore2).isEqualTo(6);
+	}
+
+	private void winGame(Set set, Player winner) {
+		for (int i = 0; i < 4; i++) {
+			matchService.scorePoint(set, winner);
+		}
+	}
+
+	private void winTieBreak(Set set, Player winner) {
+		for (int i = 0; i < 7; i++) {
+			matchService.scorePoint(set, winner);
+		}
 	}
 
 }
